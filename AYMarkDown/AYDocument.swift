258c16a358dc70f5943d2fff89ace7aa6bbab9d6
Aaron <yinzhongzheng0514@zto.com>
2020-08-06 09:03:17 +0800

    加载文件夹

---
+++ AYMarkDown/AYDocument.swift
@@ -0,0 +1,19 @@
+//
+//  AYDocument.swift
+//  AYMarkDown
+//
+//  Created by Aaron on 2020/8/5.
+//  Copyright © 2020 Aaron. All rights reserved.
+//
+
+import AppKit
+
+class AYDocument: NSDocument {
+    
+    private var _ay_data: Data?
+    
+    override func read(from data: Data, ofType typeName: String) throws {
+        
+    }
+    
+}
--- AYMarkDown/DocumentViewController.swift
+++ AYMarkDown/DocumentViewController.swift
@@ -15,27 +15,109 @@ class DocumentViewController: NSViewController {
     
     private var tableView: NSTableView {
         return scrollView.documentView as! NSTableView
-        
-        
     }
     
-    private var dataSource: [String] = []
+    private var names: [String] = []
+    private var urls: [URL] = []
+    
+    let query = NSMetadataQuery()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        dataSource = [
-            "One","One","One","One","One","One","One","One"
+        NotificationCenter.default.addObserver(self, selector: #selector(finishedGetNewDocument(_:)), name: NSNotification.Name.NSMetadataQueryDidFinishGathering, object: query)
+        loadData()
+    }
+    
+    private func loadData() {
+        query.searchScopes = [
+            NSMetadataQueryUbiquitousDocumentsScope
         ]
+//        let pred = NSPredicate(format: "%K == %@", NSMetadataItemFSNameKey, "kFILENAME")
+        query.start()
+    }
+    
+    private func addData(_ name: String, _ url: URL) {
+        names.append(name)
+        urls.append(url)
+    }
+    
+    @objc func finishedGetNewDocument(_ notification: Notification) {
+        print("current thread: ---- \(Thread.current)")
+        let query = notification.object as! NSMetadataQuery
+        query.disableUpdates()
+        query.stop()
+        if query.resultCount <= 0 {
+            return
+        }
+        for i in 0..<query.resultCount {
+            guard let item = query.result(at: i) as? NSMetadataItem else  {
+                continue
+            }
+            guard let type = item.value(forAttribute: NSMetadataItemContentTypeKey) as? String else {
+                continue
+            }
+            if type != "public.folder" {
+                continue
+            }
+            guard let displayName = item.value(forAttribute: NSMetadataItemDisplayNameKey) as? String else {
+                continue
+            }
+            guard let url = item.value(forAttribute: NSMetadataItemURLKey) as? URL else {
+                continue
+            }
+            addData(displayName, url)
+        }
+        tableView.reloadData()
+    }
+    
+    @IBAction func createDocumentAction(_ sender: Any) {
+        createDocument()
     }
     
     private func didEditTitle(row: Int, string: String) {
-        if string.count != 0 && row < dataSource.count {
-            dataSource[row] = string
+        if string.count != 0 && row < names.count {
+            names[row] = string
         }
         tableView.reloadData()
-        print(dataSource)
+    }
+    
+    private func createDocument() {
+        guard let url = checkIClould() else {
+            alert("请先开启iCloud功能")
+            return
+        }
+        let sysDocumentsURL = url.appendingPathComponent("Documents")
+        var index = 0
+        for s in names {
+            if s.hasPrefix("新建文件夹") {
+                index += 1
+            }
+        }
+        let name = index == 0 ? "新建文件夹" : "新建文件夹\(index)"
+        let newDucumentURL = sysDocumentsURL.appendingPathComponent(name)
+        
+        do {
+            try FileManager.default.createDirectory(at: newDucumentURL, withIntermediateDirectories: true, attributes: nil)
+            addData(name, newDucumentURL)
+            tableView.reloadData()
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    private func checkIClould() -> URL? {
+        FileManager.default.url(forUbiquityContainerIdentifier: "iCloud.com.aaron.brain")
+    }
+    
+    private func alert(_ message: String) {
+        let alert = NSAlert()
+        alert.messageText = "提示"
+        alert.informativeText = message
+        alert.addButton(withTitle: "确定")
+        alert.runModal()
     }
     
 }
@@ -43,7 +125,7 @@ class DocumentViewController: NSViewController {
 extension DocumentViewController: NSTableViewDataSource, NSTableViewDelegate {
     
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return dataSource.count
+        return names.count
     }
     
     func tableView(_ tableView: NSTableView, heightOfRow row: Int) -> CGFloat {
@@ -55,7 +137,7 @@ extension DocumentViewController: NSTableViewDataSource, NSTableViewDelegate {
         if (view == nil) {
             view = DocumentTableCell(frame: .zero)
         }
-        view?.textField.stringValue = dataSource[row]
+        view?.textField.stringValue = names[row]
         unowned let unownedView = view
         view?.textField.endEditClosure = { [unowned self] (string) in
             if let vw = unownedView {
@@ -122,7 +204,7 @@ class DocumentTextField: NSTextField {
         super.init(coder: coder)
         commonInit()
     }
-        
+    
     private func commonInit() {
         isBordered = false
         bezelStyle = .squareBezel
